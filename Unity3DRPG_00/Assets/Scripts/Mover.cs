4a0fd6548bcdd0453dca58a7db386ad54098b1c5
kimhyeok <kimmang940715@naver.com>
2023-08-01 12:51:51 +0900

    Nav Mesh Distination 을 사용 할 경우 window - ai - Navigation - bake (해당 위치 ) 를 해야 작동

--- Unity3DRPG_00/Assets/Scripts/Mover.cs
+++ Unity3DRPG_00/Assets/Scripts/Mover.cs
@@ -1,18 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform target;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
