84c0c5a4206c6616e872414bb4ec4af3f32b01a4
kimhyeok <kimmang940715@naver.com>
2023-08-03 15:58:55 +0900

    add Ray

--- Unity3DRPG_00/Assets/Scripts/Mover.cs
+++ Unity3DRPG_00/Assets/Scripts/Mover.cs
@@ -8,8 +8,14 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform target;
 
+    private Ray lastRay;
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
